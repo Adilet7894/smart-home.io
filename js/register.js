968fa1a3869ada335278dd6fe32dd07e7ab3ebfa
Adilet7894 <62827718+Adilet7894@users.noreply.github.com>
2020-05-18 16:31:34 +0600

    init

---
+++ js/register.js
@@ -0,0 +1,68 @@
+$(function(){
+	$.validator.setDefaults({
+		highlight: function(element){
+			$(element)
+			.closest('.form-group')
+			.addClass('has-error')
+		},
+		unhighlight: function(element){
+			$(element)
+			.closest('.form-group')
+			.removeClass('has-error')
+		}
+	});
+	
+	$.validate({
+		rules:
+		{	firstName: "required",
+			lastName: "required",
+		    password: "required",
+			birthDate: "required",
+			weight: {
+			    required:true,
+			    number:true
+			},
+			height:  {
+			    required:true,
+			    number:true
+			},
+			email: {
+				required: true,
+				email: true
+			}
+		},
+			messages:{			
+				email: {
+				required: true,
+				email: true
+			}
+		},
+				
+				firstName: {
+				required: " Please enter your name"
+				},
+				lastName: {
+					required: " Please enter your lastname"
+					},
+				password: {
+					required: " Please enter password"
+				},
+				birthDate: {
+					required: " Please enter birthdate"
+				},
+				email: {
+					required: ' Please enter email',
+					email: ' Please enter valid email'
+				},
+				weight: {
+					required: " Please enter your weight",
+					number: " Only numbers allowed"
+				},
+				height: {
+					required: " Please enter your height",
+					number: " Only numbers allowed"
+				},
+			
+			
+	});
+});
